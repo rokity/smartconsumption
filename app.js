755bf7accde8d3b31e8b2c6f1f5432bd029d50f3
rokity <riccardo.amadio1@gmail.com>
2018-05-19 18:42:28 +0200

    remove express because it doesn't have docs generator

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 //Configure Logs
 var log = require('pino')()
+
+//Configure Swagger-UI Documentation
+var swaggerUi = require('swagger-ui-express');
+var swaggerDocument = require('./swagger.json');
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+
 app.get('/', (req, res) => res.send('Hello World!'))
 
 //Get configuration variable for web server
