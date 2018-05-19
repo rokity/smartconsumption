64cf1107377297396d0bde0d58c5d9f903d11490
rokity <riccardo.amadio1@gmail.com>
2018-05-19 18:05:25 +0200

    comment

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const app = express()
 var log = require('pino')()
 app.get('/', (req, res) => res.send('Hello World!'))
 
+//Get configuration variable for web server
 var hostname = process.argv[2];
 var port = process.argv[3];
-app.listen(port,hostname, () => log.error(`Example app listening on ${hostname}:${port}!`))
+//Let's start the webserver
+app.listen(port, hostname, () => log.info(`Example app listening on ${hostname}:${port}!`))
