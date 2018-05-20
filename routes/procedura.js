90e0d758cc43a8981f1d06fba1c4da248e24897c
rokity <riccardo.amadio1@gmail.com>
2018-05-20 10:57:09 +0200

    Joi validation added

--- routes/procedura.js
+++ routes/procedura.js
@@ -1,15 +1,24 @@
 
 var mongoose = require('mongoose');
-
+var Joi = require('joi');
 module.exports = [
     { 
         method: 'GET',
-        path: '/',
+        path: '/api/procedura/insert/{name}/{TipoProcedura}',
         handler: (request,h) => 
         {     
             var Procedura = mongoose.model('Procedura');        
-            var newProcedura = new Procedura({Name:'Test',TipoProcedura:2,CreatedOn:Date.now(),Modified:Date.now(),Disabled:false});
+            var newProcedura = new Procedura({Name:request.params.name,TipoProcedura:request.params.TipoProcedura,CreatedOn:Date.now(),Modified:Date.now(),Disabled:false});
             var prom = newProcedura.save();
             return  prom;
-        } },
+        },
+        options: {
+            validate: {
+                params: {
+                    name: Joi.string().min(3),
+                    TipoProcedura: Joi.number()
+                }
+            }
+        }
+    },
 ];
