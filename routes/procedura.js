ba3594deb0b31e7cead51c5976393a20efca6282
rokity <riccardo.amadio1@gmail.com>
2018-05-20 17:02:09 +0200

    api procedura done

--- routes/procedura.js
+++ routes/procedura.js
@@ -1,28 +1,127 @@
 
-var mongoose = require('mongoose');
-var Joi = require('joi');
+const mongoose = require('mongoose');
+const Joi = require('joi');
+const Boom = require('boom');
+const log = require('pino')();
 module.exports = [
-    { 
-        method: 'GET',
-        path: '/api/procedura/insert/{name}/{TipoProcedura}',
-        handler: (request,h) => 
-        {     
-            var Procedura = mongoose.model('Procedura');        
-            var newProcedura = new Procedura({
-             Name:request.params.name,
-             TipoProcedura:request.params.TipoProcedura,
-             CreatedOn:Date.now(),
-             Modified:Date.now(),
-             Disabled:false});
-            return  newProcedura.save();
-        },
-        options: {
-            validate: {
-                params: {
-                    name: Joi.string().min(3).required(),
-                    TipoProcedura: Joi.number().required()
-                }
-            }
-        }
+  {
+    method: 'GET',
+    path: '/api/procedura/insert/{name}/{TipoProcedura}',
+    handler: (req,h) => {
+      h.type ="application/json"
+      const Procedura = mongoose.model('Procedura');
+      const newProcedura = new Procedura({
+        Name: req.params.name,
+        TipoProcedura: req.params.TipoProcedura,
+        CreatedOn: Date.now(),
+        Modified: Date.now(),
+        Disabled: false,
+      });
+      return newProcedura.save();
     },
+    options: {
+      validate: {
+        params: {
+          name: Joi.string().min(3).required(),
+          TipoProcedura: Joi.number().required(),
+        },
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/api/procedura/update/{id}/{name}/{TipoProcedura}',
+    handler: (req,h) => {
+       return new Promise((resolve,reject)=>
+      {
+        const Procedura = mongoose.model('Procedura');
+        Procedura.findOneAndUpdate({_id:req.params.id},
+          { 
+            Name: req.params.name,
+            TipoProcedura: req.params.TipoProcedura,
+            Modified:Date.now()
+           },(err,doc)=>{
+             if(err)
+              reject();
+             else
+              resolve();
+           })
+      }).then(()=>
+      {
+        return h.response().code(200)
+      })
+      .catch(()=>
+      {
+        throw Boom.badRequest('Unsupported parameter');
+      })
+    },
+    options: {
+      validate: {
+        params: {
+          id: Joi.string().required(),
+          name: Joi.string().min(3),
+          TipoProcedura: Joi.number(),
+        },
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/api/procedura/delete/{id}',
+    handler: (req,h) => {
+      return new Promise((resolve,reject)=>
+      {
+        const Procedura = mongoose.model('Procedura');
+        Procedura.findOneAndUpdate({_id:req.params.id},
+          { 
+            Disabled:true,
+            Modified:Date.now()
+           },(err,doc)=>{
+             if(err)
+              reject();
+             else
+              resolve();
+           })
+      }).then(()=>
+      {
+        return h.response().code(200)
+      })
+      .catch(()=>
+      {
+        throw Boom.badRequest('Unsupported parameter');
+      })
+    },
+    options: {
+      validate: {
+        params: {
+          id: Joi.string().required(),
+        },
+      },
+    },
+  },
+  {
+    method: 'GET',
+    path: '/api/procedura/getall',
+    handler: (req,h) => {
+      h.type ="application/json";
+      var Procedura = mongoose.model('Procedura');
+      return Procedura.find({Disabled:false}).exec(); 
+    }
+  },
+  {
+    method: 'GET',
+    path: '/api/procedura/getbyid/{id}',
+    handler: (req,h) => {
+      h.type ="application/json"
+      var Procedura = mongoose.model('Procedura');
+      return Procedura.find({_id:req.params.id,Disabled:false}).exec(); 
+    },
+    options: {
+      validate: {
+        params: {
+          id: Joi.string().required(),
+        },
+      },
+    },
+  },
 ];
