9030211935e0384b26fe98bc0cb6ef8fd2069b26
rokity <riccardo.amadio1@gmail.com>
2018-08-18 17:16:50 +0200

    refactoring after authentication module implementation

--- routes/utente.js
+++ routes/utente.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose');
 const Joi = require('joi');
+var Boom = require('boom')
+var Token = require('./../token');
 
-module.exports = [
-    {
+module.exports = [{
         method: 'POST',
-        path: '/api/utente/insert',
-        handler: (req, h) => 
-        {
+        path: '/api/utente/registrazione',
+        handler: (req, h) => {
             h.type = 'application/json';
             var Utente = mongoose.model('Utente');
             var newUtente = new Utente({
@@ -17,55 +17,94 @@ module.exports = [
                 Disabled: false,
             });
             return newUtente.save()
-            .then((doc) => {return h.response(JSON.stringify(doc)).code(200)})
-            .catch((err)=>{ return h.response(JSON.stringify({error:err})).code(400)});
+                .then((doc) => {
+                    return Token.tokenGenerator().then((value) => {
+                        global.tokens[value] = {
+                            account: utente,
+                            expireDate: Token.expireDateGenerator()
+                        };
+                        return h.response(JSON.stringify({
+                            token: value
+                        })).code(200)
+                    })
+                })
+                .catch((err) => {
+                    return h.response(JSON.stringify({
+                        error: err
+                    })).code(400)
+                });
         },
-        options:{
-            cors :true,
+        options: {
+            cors: true,
             validate: {
                 payload: {
-                  Username: Joi.string().min(5).required(),
-                  Password: Joi.string().min(5).required(),
+                    Username: Joi.string().min(5).required(),
+                    Password: Joi.string().min(5).required(),
                 },
-              },
+            },
         }
-      },
-      {
+    },
+    {
         method: 'GET',
-        path: '/api/utente/getusernamebyid/{id}',
-        handler: (req, h) => 
-        {
-            h.type = 'application/json';
-            var Utente = mongoose.model('Utente');
-            return Utente.find({ _id: req.params.id, Disabled: false },'Username').exec();
+        path: '/api/utente/getusername/{token}',
+        handler: (req, h) => {
+            if (Token.isAuthenticated(req.params)) {
+                h.type = 'application/json';
+                var Utente = mongoose.model('Utente');
+                var id = global.tokens[req.params.token].account._id;
+                return Utente.find({
+                    _id: id,
+                    Disabled: false
+                }, 'Username').exec();
+            } else
+                throw Boom.notFound('Cannot find the requested page')
         },
-        options:{
-            cors :true,
+        options: {
+            cors: true,
             validate: {
                 params: {
-                  id: Joi.string().required(),
+                    token: Joi.string().required(),
                 },
-              },
+            },
         }
-      },
-      {
+    },
+    {
         method: 'GET',
         path: '/api/utente/login/{Username}/{Password}',
-        handler: (req, h) => 
-        {
+        handler: (req, h) => {
             h.type = 'application/json';
             var Utente = mongoose.model('Utente');
-            return Utente.findOne({ Username: req.params.Username,Password:req.params.Password, Disabled: false }).exec();
+
+            return Utente.findOne({
+                    Username: req.params.Username,
+                    Password: req.params.Password,
+                    Disabled: false
+                }).exec()
+                .then((utente) => {
+                    if (utente != null) {
+                        return Token.tokenGenerator().then((value) => {
+                            global.tokens[value] = {
+                                account: utente,
+                                expireDate: Token.expireDateGenerator()
+                            };
+                            return h.response(JSON.stringify({
+                                token: value
+                            })).code(200)
+                        })
+                    } else
+                        throw Boom.notFound('Cannot find the requested page')
+                });
+
         },
-        options:{
-            cors :true,
+        options: {
+            cors: true,
             validate: {
                 params: {
                     Username: Joi.string().min(5).required(),
                     Password: Joi.string().min(5).required(),
                 },
-              },
+            },
         }
-      },
-      
+    },
+
 ]
